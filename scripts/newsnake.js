dde9cd026ae0bbedc5c9f89a15da11c3d311d014
Leerlandais <leerlandais@gmail.com>
2023-12-12 10:31:49 +0100

    added comments

--- scripts/newsnake.js
+++ scripts/newsnake.js
@@ -1,34 +1,35 @@
-const bigGrid = 396;
-const bigGridFind = document.getElementById("snakeSet");
-const startBut = document.getElementById("snakeStart");
-const snakeGridArray = [];
-var snakeLength = 5;
-var snakePos = [];
+const bigGrid = 396;                                                    // sets size of grid. Width controlled using CSS, set to 725px
+const bigGridFind = document.getElementById("snakeSet");                // locates the area where the grid will be placed
+const startBut = document.getElementById("snakeStart");                 // locates the start button
+const snakeGridArray = [];                                              // array to hold the grid
+var snakeLength = 5;                                                    // starting length of snake
+var snakePos = [];                                                      // array to hold snake's position
 for (i = 0; i < snakeLength; i++) {
-    snakePos[i] = i+206; 
+    snakePos[i] = i+206;                                                // place the snake at the middle of the grid
 }
-var newSnakePos = [];
-var snakeDir = "";
-var gridFood = new Set();
-var snakeHead = snakePos[0];
+var newSnakePos = [];                                                   // array used to calculate snake movement
+var snakeDir = "";                                                      // used to hold current direction
+var gridFood = new Set();                                               // array to store food positions
+var snakeHead = snakePos[0];                                            // used to hold current position of snake's head
 var deadRim = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 43, 44, 65, 66, 87, 88, 109, 110, 131, 132, 153, 154, 175, 176, 197, 198, 219, 220, 241, 242, 263, 264, 285, 286, 307, 308, 329, 330, 351, 352, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395];
-var snakeCollide = "";
-for (let i = 0; i < bigGrid; i++) {
-    const placeDivs = document.createElement("div");
-    placeDivs.classList.add("snakeGrid");
-    placeDivs.id = (i);
-    bigGridFind.appendChild(placeDivs);
-    snakeGridArray.push(placeDivs.id)
-//    placeDivs.textContent = placeDivs.id;
+// very annoying array to type out, needed to control the outer rim
+var snakeCollide = [];                                                  // used to check if the snake runs into itself                                
+for (let i = 0; i < bigGrid; i++) {                                     // now that all that is done, time to make the grid
+    const placeDivs = document.createElement("div");                    // creates the <div> elements
+    placeDivs.classList.add("snakeGrid");                               // adds class to each
+    placeDivs.id = (i);                                                 // and an indiviual id
+    bigGridFind.appendChild(placeDivs);                                 // puts the divs in place
+    snakeGridArray.push(placeDivs.id)                                   // adds the div ids to the grid Array
+//   placeDivs.textContent = placeDivs.id;
 }
-deadRim.forEach(item => {
+deadRim.forEach(item => {                                               // colours the outer rim red
     document.getElementById(snakeGridArray[item]).style.backgroundColor = "red";
 })
 console.log(snakeGridArray);
 
-startBut.addEventListener("click", makeFood);
+startBut.addEventListener("click", makeFood);                           // waits for start to be pressed
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", (e) => {                           // Listens for arrow button presses and sends relevant direction to move snake function
     if (e.key === "ArrowUp") {
         console.log("up arrow pressed");
         moveSnake("sUp");
@@ -44,15 +45,15 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
-function makeFood() {
-    while (gridFood.size < 18){
-        var foodSquare = Math.floor(Math.random() * 400)
-          if (!gridFood.has(foodSquare) && !snakePos.includes(foodSquare) && !deadRim.includes(foodSquare)){
-             gridFood.add(foodSquare);
+function makeFood() {                                                   // time to place some food
+    while (gridFood.size < 18){                                         // adjust this to change how much to place
+        var foodSquare = Math.floor(Math.random() * 396)
+          if (!gridFood.has(foodSquare) && !snakePos.includes(foodSquare) && !deadRim.includes(foodSquare)){    // makes sure the random selection is unique, isn't where the snake is and isn't in the outer rim
+             gridFood.add(foodSquare);                                                                          // adds it all to the array
           }
       }
         gridFood.forEach(item => {
-        document.getElementById(snakeGridArray[item]).style.backgroundColor = "green";
+        document.getElementById(snakeGridArray[item]).style.backgroundColor = "green";                          // and colours the squares
       });
       console.log("Food Placed", gridFood);
       makeSnake;
@@ -60,49 +61,49 @@ function makeFood() {
 
 function makeSnake() {
     console.log(snakeHead);
-    snakeCollide = snakePos.slice(1);
-    if (deadRim.includes(snakeHead) || snakeCollide.includes(snakeHead)) {
-        confirm("You died. You grew your snake to " + snakePos.length);
+    snakeCollide = snakePos.slice(1);                                                                           // copies the current snake position (minus the head) to the collide array
+    if (deadRim.includes(snakeHead) || snakeCollide.includes(snakeHead)) {                                      // checks if the snake's head has reached the outer rim or collided with its body
+        confirm("You died. You grew your snake to " + snakePos.length);                                         // You're dead. Also shows snake's length
         location.reload();
     }
-     newSnakePos = [];
+     newSnakePos = [];                                                                                          // makes sure this is empty
     //  startBut.disabled = true;
     console.log ("here we are : ", snakePos);
     for (let i = 0; i < snakePos.length; i++) {
-        document.getElementById(snakePos[i]).style.backgroundColor = "blue";
+        document.getElementById(snakePos[i]).style.backgroundColor = "blue";                                    // colours the snake
     }
-    if (gridFood.has(snakePos[0])) {
-        gridFood.delete(snakePos[0]);
+    if (gridFood.has(snakePos[0])) {                                                                            // checks if the snake is eating. When I made this part, I hadn't yet made a var for snakeHead
+        gridFood.delete(snakePos[0]);                                                                           // removes the food once eaten 
         console.log ("He ate!");
-        snakePos.push(snakePos[snakePos.length-1] - snakeDir);          
+        snakePos.push(snakePos[snakePos.length-1] - snakeDir);                                                  // adds a new square to the end of snake's position depending on snake's current direction
         console.log("dir : ", snakeDir, "pos : ", snakePos);
     }
     
 }
 
-var intervalId = null;
+var intervalId = null;                                                                                          // var to control which direction will be repeated
 function moveSnake(dir) {
-    clearInterval(intervalId);
+    clearInterval(intervalId);                                                                                  // stops repetition of previous direction (if any)
     if (dir === "sUp") {
-        intervalId = setInterval(() => {
+        intervalId = setInterval(() => {                                                                        // starts the interval repetition
         console.log("let's move up");
-        newSnakePos[0] = (snakePos[0]) - 22;
-        snakeDir = - 22;
-        document.getElementById(snakePos[snakePos.length-1]).style.backgroundColor = "";
+        newSnakePos[0] = (snakePos[0]) - 22;                                                                    // moves the snakes head position to the square immediately above
+        snakeDir = - 22;                                                                                        // stores the current direction
+        document.getElementById(snakePos[snakePos.length-1]).style.backgroundColor = "";                        // deletes the last square of the snake
            for (let i = 0; i < snakePos.length - 1; i++) {
-               document.getElementById(snakePos[i]).style.backgroundColor = "";
-               newSnakePos.push(snakePos[i]);
+               document.getElementById(snakePos[i]).style.backgroundColor = "";                                 // deletes the rest of the snake
+               newSnakePos.push(snakePos[i]);                                                                   // and makes the updated position of the snake
                }
                snakePos = newSnakePos;
                console.log ("new position : ", snakePos);
-               snakeHead = snakePos[0];
-                makeSnake();
-            }, 100);
+               snakeHead = snakePos[0];                                                                         // stored for collision detection
+                makeSnake();                                                                                    // jump back to here to colour the new snake
+            }, 100);                                                                                            // and repeat 10 times per second
             }else if (dir === "sDown") {
                 intervalId = setInterval(() => {
     
                 console.log("let's move down");
-        newSnakePos[0] = (snakePos[0]) + 22;
+        newSnakePos[0] = (snakePos[0]) + 22;                                                                    // same as line 90 but moves to the square immediately below
         snakeDir = + 22;
         document.getElementById(snakePos[snakePos.length-1]).style.backgroundColor = "";
         for (let i = 0; i < snakePos.length - 1; i++) {
@@ -117,7 +118,7 @@ function moveSnake(dir) {
     }else if (dir === "sLeft") {
         intervalId = setInterval(() => {
         console.log("let's move left");
-        newSnakePos[0] = (snakePos[0]) - 1;
+        newSnakePos[0] = (snakePos[0]) - 1;                                                                      // line 90 again but this time to the left
         snakeDir = - 1;
         document.getElementById(snakePos[snakePos.length-1]).style.backgroundColor = "";
         for (let i = 0; i < snakePos.length - 1; i++) {
@@ -132,7 +133,7 @@ function moveSnake(dir) {
     }else if (dir === "sRight") {
         intervalId = setInterval(() => {
         console.log("let's move right");
-        newSnakePos[0] = (snakePos[0]) + 1;
+        newSnakePos[0] = (snakePos[0]) + 1;                                                                       // and, of course, to the right
         snakeDir = + 1;
         document.getElementById(snakePos[snakePos.length-1]).style.backgroundColor = "";
         for (let i = 0; i < snakePos.length - 1; i++) {
